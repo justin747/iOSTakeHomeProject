b8c794b676f0c8d989ba7b2a47e785fe8319facb
justin747 <justin747@gmail.com>
2022-08-03 19:52:27 -0700

    Initial Commit

---
+++ iOSTakeHomeProject/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  iOSTakeHomeProject
+//
+//  Created by Justin on 8/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSTakeHomeProject/iOSTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTakeHomeProjectApp.swift
+//  iOSTakeHomeProject
+//
+//  Created by Justin on 8/3/22.
+//
+
+import SwiftUI
+
+@main
+struct iOSTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
