1429316327dd08c06d00cad0986b80d1a8a5256e
justin747 <justin747@gmail.com>
2022-08-04 11:55:56 -0700

    Added Color and Theme enums
    Added Color and Theme enums for type safety.
    
    Added MVVM folders to each individual Feature

---
+++ iOSTakeHomeProject/Resources/Symbols.swift
@@ -0,0 +1,15 @@
+//
+//  Symbols.swift
+//  iOSTakeHomeProject
+//
+//  Created by Justin on 8/3/22.
+//
+
+import Foundation
+import SwiftUI
+
+enum Symbols {
+    static let person = Image(systemName: "person.2")
+    static let gear   = Image(systemName: "gear")
+    static let plus   = Image(systemName: "plus")
+}
