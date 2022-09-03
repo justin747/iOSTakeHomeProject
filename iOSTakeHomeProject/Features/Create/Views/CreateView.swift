f81b08663db554d11fa712da3a7aebe66a879a36
justin747 <justin747@gmail.com>
2022-09-02 21:57:52 -0700

    Lazy Update
    Lazy Update - LWD

--- iOSTakeHomeProject/Features/Create/Views/CreateView.swift
+++ iOSTakeHomeProject/Features/Create/Views/CreateView.swift
@@ -50,7 +50,7 @@ private extension CreateView {
     }
     
     var firstName: some View {
-        TextField("First Name", text: .constant("First"))
+        TextField("First Name", text: .constant(""))
     }
     
     var lastName: some View {
