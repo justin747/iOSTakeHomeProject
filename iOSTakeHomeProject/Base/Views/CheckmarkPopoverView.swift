072d11d167a4560b980eecbf26ce59be90160a49
justin747 <justin747@gmail.com>
2022-09-14 15:45:17 -0700

    Created CheckmarkPopoverView
    Created CheckmarkPopoverView to display animated checkmark when a successful contact has been created.

---
+++ iOSTakeHomeProject/Base/Views/CheckmarkPopoverView.swift
@@ -0,0 +1,20 @@
+//
+//  CheckmarkPopoverView.swift
+//  iOSTakeHomeProject
+//
+//  Created by Justin on 9/13/22.
+//
+
+import SwiftUI
+
+struct CheckmarkPopoverView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckmarkPopoverView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckmarkPopoverView()
+    }
+}
