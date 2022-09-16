f3134b12b26d3c53f523624791c2b4f8ee164521
justin747 <justin747@gmail.com>
2022-09-15 18:54:37 -0700

    Added Checkmark Symbol
    Added Checkmark Symbol

--- iOSTakeHomeProject/Resources/Symbols.swift
+++ iOSTakeHomeProject/Resources/Symbols.swift
@@ -9,8 +9,9 @@ import Foundation
 import SwiftUI
 
 enum Symbols {
-    static let person = Image(systemName: "person.2")
-    static let gear   = Image(systemName: "gear")
-    static let plus   = Image(systemName: "plus")
-    static let link   = Image(systemName: "link")
+    static let person    = Image(systemName: "person.2")
+    static let gear      = Image(systemName: "gear")
+    static let plus      = Image(systemName: "plus")
+    static let link      = Image(systemName: "link")
+    static let checkmark = Image(systemName: "checkmark")
 }
