112700e5d4ef270527b07193484c2ae28c3494d3
justin747 <justin747@gmail.com>
2022-09-16 20:06:44 -0700

    Added Checkmark to Popover View
    Added Checkmark to Popover View

--- iOSTakeHomeProject/Base/Views/CheckmarkPopoverView.swift
+++ iOSTakeHomeProject/Base/Views/CheckmarkPopoverView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct CheckmarkPopoverView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Symbo.checkmark
+            .font(.system(.largeTitle, design: .rounded).bold())
     }
 }
 
