c022a8dfcad3b07f7c4f21d2cfaefa37bbc6ae6b
justin747 <justin747@gmail.com>
2022-10-05 19:27:36 -0700

    Added base functions
    Added base functions endpoint test

--- iOSTakeHomeProjectTests/Networking/Endpoint/NetworkingEndpointTests.swift
+++ iOSTakeHomeProjectTests/Networking/Endpoint/NetworkingEndpointTests.swift
@@ -10,4 +10,15 @@ import XCTest
 final class NetworkingEndpointTests: XCTestCase {
 
 
+    func test_with_people_endpoint_request_is_valid() {
+        
+    }
+    
+    func test_with_detail_endpoint_request_is_valid() {
+        
+    }
+    
+    func test_with_create_endpoint_request_is_valid() {
+        
+    }
 }
